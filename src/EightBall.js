378d638d8b17571b7ef194b2bb93e93469acb7ef
Rostyslav Kravchenko <rostyk@live.com>
2023-12-13 14:04:46 -0500

    first commit

---
+++ src/EightBall.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const answers = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+
+const defaultAnswer = { msg:"Think of a Question", color:"black", textcolor: "white"};
+
+function Circle(props) {
+  const [answer, setAnswer] = useState(defaultAnswer);
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    setAnswer(answers[randomIndex]);
+  };
+
+  return (
+    <div className="Circle" style={{ backgroundColor: answer.color }} onClick={handleClick}>
+      {answer.msg}
+    </div>
+  );
+}
+
+export default Circle;
